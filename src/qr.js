64da8b762085465ee2a43817e1b858f124eec636
Ikjot Singh Dhody <55295613+ikjot-2605@users.noreply.github.com>
2021-06-08 01:32:14 +0530

    final bit stream excluding ecc

--- src/qr.js
+++ src/qr.js
@@ -3,6 +3,7 @@ import { QRCodeModes } from "./qr_code_modes.js";
 import { BitHandlingUtility } from "./bit_handling_util.js";
 import { versionFittingJson } from "./version_fitting.js";
 
+const bitHandlingUtilty = new BitHandlingUtility();
 class QRCode {
     constructor(data, error_correction_level) {
         // this data is the string to be encoded
@@ -24,13 +25,12 @@ class QRCode {
                 let currentNumber = parseInt(
                     data.substring(i, i + 3 < dataLength ? i + 3 : dataLength)
                 );
-                const bitHandlingUtilty = new BitHandlingUtility();
                 resultantCompleteBitString =
                     resultantCompleteBitString +
                     bitHandlingUtilty.getEncodedBitStringFromNumber(
                         currentNumber,
-                        10,
-                        i + 3 < dataLength
+                        i + 3 < dataLength?10:currentNumber.toString().length==1?4:7,
+                        true
                     );
             }
             this.basic_data_bit_stream = resultantCompleteBitString;
@@ -45,14 +45,18 @@ class QRCode {
 
     addModeAndCountBits() {
         this.final_data_bit_stream =
-            QRCodeModes[this.mode] + this.data.length + this.basic_data_bit_stream;
+            QRCodeModes[this.mode] + bitHandlingUtilty.getEncodedBitStringFromNumber(
+                this.data.length,
+                10,
+                true
+            ) + this.basic_data_bit_stream;
     }
 
     addTerminatorAndBitPadding() {
         this.final_data_bit_stream = this.final_data_bit_stream + "0000";
-        let bitPaddingLength = this.final_data_bit_stream.length % 8;
+        let bitPaddingLength = 8 - (this.final_data_bit_stream.length % 8);
         for (let i = 0; i < bitPaddingLength; i++) {
-            this.final_data_bit_stream.concat("0");
+            this.final_data_bit_stream = this.final_data_bit_stream + "0";
         }
     }
 
@@ -61,12 +65,23 @@ class QRCode {
         for(key in versionFittingJson){
             let information = versionFittingJson[key];
             if (
-                parseInt(information[this.error_correction_level]) >=
+                parseInt(information[this.error_correction_level.toString()]) >=
                 Math.floor(this.final_data_bit_stream.length / 8)
             ) {
                 this.version = parseInt(key);
+                break;
             }
         }
+    }
+
+    addBytePadding(){
+        let information = versionFittingJson[this.version.toString()];
+        let numberOfBytesTillNow = Math.floor(this.final_data_bit_stream.length/8);
+        let remainingBytes = parseInt(information[this.error_correction_level]) -numberOfBytesTillNow;
+        for(let i = 0;i<remainingBytes/2;i++){
+            this.final_data_bit_stream = this.final_data_bit_stream + "1110110000010001";
+        }
+        if(remainingBytes%2!=0)this.final_data_bit_stream = this.final_data_bit_stream + "11101100";
     }
 }
 
@@ -76,4 +91,6 @@ qr.encodeDataToBitStream(qr.mode, qr.data);
 qr.addModeAndCountBits();
 qr.addTerminatorAndBitPadding();
 qr.fitDataToVersion();
+console.log(qr.final_data_bit_stream);
+qr.addBytePadding();
 console.log(qr.final_data_bit_stream);
