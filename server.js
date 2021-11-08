cb9c90d08358a947fef07a61efac67d3eb873a72
Ikjot Singh <ikjotsd@gmail.com>
2021-11-08 21:39:42 +0530

    cors enabled

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
+var cors = require('cors')
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser');
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
